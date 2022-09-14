89fbad70d3f15a1cb8fd31a144d648e4d9700c1b
te6lim <teslimakande@gmail.com>
2022-09-14 17:43:46 +0100

    initial commit

---
+++ app/src/main/java/com/te6lim/c2candroidassessment/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.te6lim.c2candroidassessment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
